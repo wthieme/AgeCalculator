9cadd47eefbf1eddb7203550324f46bb1d0ca4e0
wilhelm.thieme@gmail.com <Tubifex2016!>
2019-10-13 10:29:46 +0200

    Update libraries and AndroidX

--- app/src/main/java/nl/whitedove/agecalculator/ListDatesActivity.java
+++ app/src/main/java/nl/whitedove/agecalculator/ListDatesActivity.java
@@ -3,7 +3,7 @@ package nl.whitedove.agecalculator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
